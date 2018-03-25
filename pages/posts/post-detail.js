6dc77fa377d3e5ff9ad0a78103e5c7cc34d7aa32
moTzxx <JingYin007@users.noreply.github.com>
2018-03-26 01:05:31 +0800

    post-detail
    文章详情页的跳转准备

--- pages/posts/post-detail.js
+++ pages/posts/post-detail.js
@@ -1,78 +1,66 @@
-// pages/posts/post.js
-//注意 只能使用相对路径
-var postsData_js = require('../../data/posts-data.js')
+// pages/posts/post-detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+  
   },
-
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
-    this.setData({
-      swiperImg: postsData_js.swiperImg,
-      objData: postsData_js.objData
-    });
-  },
-  onPostTap:function(event){
-    var postId = event.currentTarget.dataset.postId;
-    wx.navigateTo({
-      url: 'post-detail/post-detail?id='+postId,
-    })
+  onLoad: function (options) {
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    //console.log('onReady');
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    //console.log('onShow');
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    //console.log('onHide');
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    //console.log('onUnload');
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    //console.log('onPullDownRefresh');
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    //console.log('onReachBottom');
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    //console.log('onShareAppMessage');
+  
   }
 })
