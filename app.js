bf5e55c7abaa8157d26f80d42c9d06a371a4ba97
moTzxx <JingYin007@users.noreply.github.com>
2018-04-10 02:25:32 +0800

    request
    请求电影数据

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ App({
   globalData: {
     g_currentMusicPostId: null,
     g_isPlayingMusic: false,
-
+    doubanBase: "http://t.yushu.im"
   },
 })
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,20 +1,50 @@
 // pages/movies/movies.js
+var app=getApp();
 Page({
+  //RESTFUL API JSON
+  //SOAP XML
+
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    inTheaters:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var doubanBase = app.globalData.doubanBase;
+    var in_theatersUrl = doubanBase + '/v2/movie/in_theaters' + '?start=0&count=3';
+    var coming_soonUrl = doubanBase + '/v2/movie/coming_soon' + '?start=0&count=3';
+    var top250Url = doubanBase + '/v2/movie/top250' + '?start=0&count=3';
+    this.getMoviesList(doubanBase, in_theatersUrl, 'inTheaters');
+    this.getMoviesList(doubanBase, coming_soonUrl, 'comingSoon');
+    this.getMoviesList(doubanBase, top250Url, 'top250');
   },
 
+  getMoviesList: function (doubanBase, in_theatersUrl,dataTag){
+    var that = this;
+    wx.request({
+      url: in_theatersUrl,
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        that.setData({
+          dataTag: res.data.subjects,
+        })
+        console.log(dataTag+':');
+        console.log(res);
+      },
+      fail: function (error) {
+        console.log(error);
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
