0b7a7127ec26074244aafbe591c38f9c06e99fc5
moTzxx <JingYin007@users.noreply.github.com>
2018-05-02 16:48:32 +0800

    ok

--- pages/movies/more-movies/more-movies.js
+++ pages/movies/more-movies/more-movies.js
@@ -6,7 +6,7 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
+ data: {
     movies: {},
     nextUrl: '',
     totalCount: 0,
@@ -25,35 +25,35 @@ Page({
     this.setData({
       nextUrl: requestMoviesUrl,
     })
-    util.http(requestMoviesUrl, this.processDoubanData);
-    //wx.startPullDownRefresh();
+    util.http(requestMoviesUrl, this.processDoubandata);
+    //wx.startPullownRefresh();
   },
 
-  //TODO 进行上滑数据加载
+  //TOO 进行上滑数据加载
   onScrollLower: function (event) {
     var nextUrl = this.data.nextUrl +
       "?start=" + this.data.totalCount + "&count=20";
-    util.http(nextUrl, this.processDoubanData);
+    util.http(nextUrl, this.processDoubandata);
     //显示 导航栏加载效果
     wx.showNavigationBarLoading();
   },
   //下拉刷新 停止当前页面下拉刷新
-  onPullDownRefresh: function (event) {
+  onPullownRefresh: function (event) {
     var refreshUrl = this.data.nextUrl + "?start=10&count=20";
     this.data.totalCount = 0;
     wx.showNavigationBarLoading()
-    util.http(refreshUrl, this.processDoubanData);
-    wx.startPullDownRefresh();
+    util.http(refreshUrl, this.processDoubandata);
+    wx.startPullownRefresh();
   },
-  processDoubanData: function (moviesDouban) {
+  processDoubandata: function (moviesDouban) {
     var movies = [];
-    for (var idx in moviesDouban.subjects) {
-      var subject = moviesDouban.subjects[idx];
+    for (var ix in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[ix];
       var title = subject.title;
       title = title.length >= 6 ? title.substring(0, 6) + "..." : title;
 
       var temp = {
-        movieId: subject.id,
+        movieI: subject.i,
         title: title,
         average: subject.rating.average,
         coverageUrl: subject.images.large,
@@ -96,7 +96,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function (options) {
-    //TODO 详情页的标题栏设置 
+    //TOO 详情页的标题栏设置 
     wx.setNavigationBarTitle({
       title: this.data.titleBarName
     })
@@ -126,7 +126,7 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullownRefresh: function () {
 
   },
 
