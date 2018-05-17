fcc9b240575340d1fc28c7d3ed0edd293eaac44a
moTzxx <JingYin007@users.noreply.github.com>
2018-05-18 00:08:14 +0800

    ok

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -59,6 +59,13 @@ Page({
       })
       app.globalData.g_isPlayingMusic = false;
       app.globalData.g_currentMusicPostId = null;
+    });
+    wx.onBackgroundAudioStop(function () {
+      that.setData({
+        isPlayingMusic: false,
+      })
+      app.globalData.g_isPlayingMusic = false;
+      app.globalData.g_currentMusicPostId = null;
     })
   },
   /**
